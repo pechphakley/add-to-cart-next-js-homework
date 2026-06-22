d45aaeb5206dc47b91e52b11c963192aaf7583e3
Pech Phakley <kmengley8@gmail.com>
2026-06-22 12:26:54 +0700

    table data render

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,11 @@ import { Providers } from "./providers";
 import Link from "next/link";
 import CartIcon from "../components/CardIcon"
 import "./globals.css";
+import { Geist } from "next/font/google";
+import { cn } from "@/lib/utils";
+
+const geist = Geist({subsets:['latin'],variable:'--font-sans'});
+
 
 export default function RootLayout({
   children,
@@ -9,7 +14,7 @@ export default function RootLayout({
   children: React.ReactNode;
 }) {
   return (
-    <html lang="en">
+    <html lang="en" className={cn("font-sans", geist.variable)}>
       <body>
         <Providers>
           <nav style={{
